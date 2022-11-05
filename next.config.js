0a55a140df7ace781e19332425bdd822c7d97cc7
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-11-05 23:48:32 +0700

    Update package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+    trailingSlash: true,
+    webpack: (config, { dev }) => {
+        // Tell webpack to ignore watching content files in the content folder.
+        // Otherwise webpack recompiles the app and refreshes the whole page.
+        // function to update the content on the page without refreshing the
+        // whole page
+        config.watchOptions.ignored.push('/page/');
+        if (dev) {
+            // enable tree shaking for development mode, on production it is on by default
+            config.optimization.usedExports = true;
+        }
+
+        return config;
+    }
+});
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   unstable_staticImage: true,
 })
+
 module.exports = withNextra()
+
+module.exports = withBundleAnalyzer()
