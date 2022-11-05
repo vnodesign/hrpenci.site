6af32922e099257f42b591073d45c1c9fb6968e7
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-11-06 02:45:10 +0700

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,24 @@
+import Script from 'next/script'
 import 'nextra-theme-docs/style.css'
 import '../src/assets/custom.css'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-RCV263F7H7"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-RCV263F7H7');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
