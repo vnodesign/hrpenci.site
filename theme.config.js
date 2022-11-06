767ce77ddaacb620195341c8d24acecb3e543197
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-11-06 21:01:51 +0700

    Update document

--- theme.config.js
+++ theme.config.js
@@ -1,29 +1,29 @@
 export default {
   github: '',
   docsRepositoryBase: '',
-  titleSuffix: '',
-  logo: <span className="inline mr-2 font-extrabold">HR Community</span>,
+  titleSuffix: ' | HR Documentation',
+  logo: <span className="inline font-extrabold">HR Documentation</span>,
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta
         name="description"
-        content="Chào mừng bạn đến với HR Community, nơi chia sẻ các khái niệm trong ngành IT và ngành Design."
+        content="Chào mừng bạn đến với HR Documentation, nơi chia sẻ các khái niệm trong ngành IT và ngành Design."
       />
-      <meta property="og:site_name" content="HR Community" />
+      <meta property="og:site_name" content="HR Documentation" />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="HR Community" />
+      <meta property="og:title" content="HR Documentation" />
       <meta
         property="og:description"
-        content="Chào mừng bạn đến với HR Community, nơi chia sẻ các khái niệm trong ngành IT và ngành Design."
+        content="Chào mừng bạn đến với HR Documentation, nơi chia sẻ các khái niệm trong ngành IT và ngành Design."
       />
       <meta property="og:url" content="https://hr.penci.me" />
       <meta property="og:image" content="/EAEC120F-BD44-45FA-8F1A-E9052824DA98.png" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="HR Community" />
+      <meta name="twitter:title" content="HR Documentation" />
       <meta
         name="twitter:description"
-        content="Chào mừng bạn đến với HR Community, nơi chia sẻ các khái niệm trong ngành IT và ngành Design."
+        content="Chào mừng bạn đến với HR Documentation, nơi chia sẻ các khái niệm trong ngành IT và ngành Design."
       />
       <meta name="twitter:url" content="https://hr.penci.me" />
       <meta name="twitter:image" content="/EAEC120F-BD44-45FA-8F1A-E9052824DA98.png" />
@@ -38,5 +38,5 @@ export default {
   darkMode: false,
   floatTOC: true,
   footerEditLink: '',
-  footerText: `Copyright © ${new Date().getFullYear()} HR Community. All Rights Reserved.`,
+  footerText: '',
 }
