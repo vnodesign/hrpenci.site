5f7a9312a9606cc62dba1b9a4c358800d6850a5d
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-11-28 01:52:12 +0700

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,6 +1,6 @@
 export default {
   github: 'https://github.com/vnodesign',
-  docsRepositoryBase: '',
+  docsRepositoryBase: 'https://github.com/vnodesign/hr-document/blob/docs'
   titleSuffix: ' | HR Documentation',
   logo: <span className="inline font-extrabold">HR Documentation</span>,
   head: (
@@ -12,14 +12,14 @@ export default {
       <link rel="canonical" href="https://hr.penci.me" />
       <meta
         name="description"
-        content="Chào mừng bạn đến với HR Documentation, nơi chia sẻ các khái niệm trong ngành IT và ngành Design."
+        content="Chào mừng bạn đến với HR Documentation, nơi chia sẻ các tài liệu trong ngành IT và ngành Design."
       />
       <meta property="og:site_name" content="HR Documentation" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="HR Documentation" />
       <meta
         property="og:description"
-        content="Chào mừng bạn đến với HR Documentation, nơi chia sẻ các khái niệm trong ngành IT và ngành Design."
+        content="Chào mừng bạn đến với HR Documentation, nơi chia sẻ các tài liệu trong ngành IT và ngành Design."
       />
       <meta property="og:url" content="https://hr.penci.me" />
       <meta property="og:image" content="/EAEC120F-BD44-45FA-8F1A-E9052824DA98.png" />
@@ -27,7 +27,7 @@ export default {
       <meta name="twitter:title" content="HR Documentation" />
       <meta
         name="twitter:description"
-        content="Chào mừng bạn đến với HR Documentation, nơi chia sẻ các khái niệm trong ngành IT và ngành Design."
+        content="Chào mừng bạn đến với HR Documentation, nơi chia sẻ các tài liệu trong ngành IT và ngành Design."
       />
       <meta name="twitter:url" content="https://hr.penci.me" />
       <meta name="twitter:image" content="/EAEC120F-BD44-45FA-8F1A-E9052824DA98.png" />
