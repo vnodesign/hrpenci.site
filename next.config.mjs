3d056979e533e12511b6af85e0ee9d1a05a36bc5
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-12-10 18:09:33 +0700

    Migrate to Preact

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,8 @@ import withPWA from 'next-pwa'
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './src/theme.config.tsx',
-  staticImage: true,
-  flexsearch: {
-    codeblock: false
-  },
-  readingTime: true,
+  unstable_flexsearch: true,
+  unstable_staticImage: true,
 })
 
 export default withNextra(withPWA({
@@ -21,5 +18,23 @@ export default withNextra(withPWA({
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === 'development'
-  }
+  },
+  webpack: (config, { dev, isServer }) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    if (!dev && !isServer) {
+      // Replace React with Preact only in client production build
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+
+    return config
+  },
 }))
