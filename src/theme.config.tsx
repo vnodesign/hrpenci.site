98c1e4f91adc8b9dfce9bd0dde627e63706ec3ad
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-12-11 02:42:36 +0700

    Update theme.config.tsx

--- src/theme.config.tsx
+++ src/theme.config.tsx
@@ -75,8 +75,6 @@ export default {
   },
   head: (
     <>
-      
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="vi" />
       <meta name="apple-mobile-web-app-title" content="HR Documentation" />
       <link rel="manifest" href="/manifest.webmanifest" />
