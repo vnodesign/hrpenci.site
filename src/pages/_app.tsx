297d24a5c7f3dbe952f1787e514c7b85789391a3
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-12-11 11:12:02 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,6 +29,21 @@ export default function Nextra({ Component, pageProps }: NextraAppProps) {
     }
   }, [router.events])
 
+  useEffect(() => {
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+       navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
+
   return (
     <SSRProvider>
       <FAQPageJsonLd />
