92081861163d0ea45b9b6bb67b16ec08d226e8ca
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-12-19 12:17:40 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx}'
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/theme.config.tsx'
   ],
   theme: {
-    extend: {
-    }
+    extend: {}
   },
-  darkMode: ['class', 'html[class~="dark"]']
+  plugins: [],
+  darkMode: 'class'
 }
