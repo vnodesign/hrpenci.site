f626ad28daa3724211000293eb092d2ab1a2f452
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-12-19 14:30:50 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -20,17 +20,15 @@ export default withNextra(withPWA({
     disable: process.env.NODE_ENV === 'development'
   },
   webpack: (config, { dev, isServer }) => {
-
+    // Replace React with Preact only in client production build
     if (!dev && !isServer) {
-      // Replace React with Preact only in client production build
       Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
         react: 'preact/compat',
         'react-dom/test-utils': 'preact/test-utils',
         'react-dom': 'preact/compat',
-      })
+      });
     }
 
-    return config
+    return config;
   },
 }))
