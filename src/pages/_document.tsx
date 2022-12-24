047c183cc8d3e76518eb19d11a1daada6aba4980
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-12-24 15:05:59 +0700

    Update style homepage

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import type { DocumentInitialProps, DocumentContext } from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return initialProps;
-  }
 
   render() {
     return (
