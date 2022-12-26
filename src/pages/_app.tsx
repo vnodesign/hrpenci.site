d90656039edcdbdbe11be0267821d7d12c2b2f96
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-12-26 09:35:32 +0700

    Fix Ga Script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,6 @@ import { SSRProvider } from '@react-aria/ssr'
 import { FAQPageJsonLd, SocialProfileJsonLd, LogoJsonLd } from 'next-seo'
 import * as gtag from '@lib/gtag'
 import { GAScript } from '@components/GoogleAnalytics'
-import Script from 'next/script'
 import '../../scss/style.scss'
 
 type NextraAppProps = AppProps & {
@@ -57,7 +56,6 @@ export default function Nextra({ Component, pageProps }: NextraAppProps) {
         url="https://hr.penci.me"
       />
       <Component {...pageProps} />
-      <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4183134625750063" />
       <GAScript id="G-RCV263F7H7" />
     </SSRProvider>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="vi" prefix="og: https://ogp.me/ns#">
-        <Head />
+        <Head>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4183134625750063"></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
