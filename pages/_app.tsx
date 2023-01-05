02857dbbd591480d1f1fda0fc529caa6fb1eb504
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-01-05 17:26:42 +0700

    Optimize _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,10 @@
 import "../scss/style.scss";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import type { AppProps } from "next/app";
-import type { ReactNode } from "react";
-import * as gtag from "@lib/gtag";
+import type { ReactElement } from 'react'
+import type { AppProps } from 'next/app'
 
-type NextraAppProps = AppProps & {
-  Component: AppProps["Component"] & {
-    getLayout: (page: ReactNode) => ReactNode;
-  };
-};
-
-export default function Nextra({ Component, pageProps }: NextraAppProps) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url: any) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    router.events.on("hashChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-      router.events.off("hashChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
+export default function Nextra({
+  Component,
+  pageProps
+}: AppProps): ReactElement {
   return <Component {...pageProps} />
 }
