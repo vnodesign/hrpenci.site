1eb0e0a6bedacd3b15fecb3edce482f205464089
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-02-19 23:17:13 +0700

    Update Google Analytics

--- components/GoogleAnalytics/index.tsx
+++ components/GoogleAnalytics/index.tsx
@@ -11,15 +11,15 @@ export function GAScript({ id }: GAProp) {
       />
 
       <Script id="gtag-script">
-      {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${id}', {
-              page_path: window.location.pathname,
-            });
-      `}
-    </Script>
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${id}', {
+                page_path: window.location.pathname,
+              });
+        `}
+      </Script>
     </>
   );
 }
