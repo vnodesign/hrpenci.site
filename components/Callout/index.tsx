c79a0236e47cf9e335af26431f9216b15790b1f1
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-03-06 01:28:42 +0700

    Update index.tsx

--- components/Callout/index.tsx
+++ components/Callout/index.tsx
@@ -2,6 +2,19 @@ import { ReactElement, ReactNode } from "react";
 import cn from "clsx";
 import { InformationCircleIcon } from "nextra/icons";
 
+const DEFAULT_CLASS = cn(
+  "bg-orange-100 border-orange-500 text-orange-700 dark:bg-orange-200 dark:text-orange-800",
+);
+const ERROR_CLASS = cn(
+  "bg-red-100 border-red-500 text-red-700 dark:bg-red-200 dark:text-red-800",
+);
+const INFO_CLASS = cn(
+  "bg-blue-100 border-blue-500 text-blue-700 dark:bg-blue-200 dark:text-blue-800",
+);
+const WARNING_CLASS = cn(
+  "bg-yellow-100 border-yellow-500 text-yellow-700 dark:bg-yellow-200 dark:text-yellow-800",
+);
+
 const TypeToEmoji = {
   default: "💡",
   error: "🚫",
@@ -10,21 +23,6 @@ const TypeToEmoji = {
 };
 
 type CalloutType = keyof typeof TypeToEmoji;
-
-const classes: Record<CalloutType, string> = {
-  default: cn(
-    "bg-orange-100 border-orange-500 text-orange-700 dark:bg-orange-200 dark:text-orange-800",
-  ),
-  error: cn(
-    "bg-red-100 border-red-500 text-red-700 dark:bg-red-200 dark:text-red-800",
-  ),
-  info: cn(
-    "bg-blue-100 border-blue-500 text-blue-700 dark:bg-blue-200 dark:text-blue-800",
-  ),
-  warning: cn(
-    "bg-yellow-100 border-yellow-500 text-yellow-700 dark:bg-yellow-200 dark:text-yellow-800",
-  ),
-};
 
 type CalloutProps = {
   type?: CalloutType;
@@ -37,11 +35,18 @@ export function Callout({
   type = "default",
   emoji = TypeToEmoji[type],
 }: CalloutProps): ReactElement {
+  const classByType = {
+    default: DEFAULT_CLASS,
+    error: ERROR_CLASS,
+    info: INFO_CLASS,
+    warning: WARNING_CLASS,
+  };
+
   return (
     <div
       className={cn(
         "my-4 mx-0 border-l-[.5rem] pt-4 pb-2 px-6 rounded-lg",
-        classes[type],
+        classByType[type],
       )}
     >
       <div className="flex m-0 text-base font-semibold">{emoji}</div>
