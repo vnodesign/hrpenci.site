d7a3af1982b6f43856c7114f5ef850044978cfd0
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-03-10 01:21:11 +0700

    Fix styled

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    'node_modules/nextra-theme-docs/dist**/*',
+    './theme.config.tsx',
   ],
   theme: {
     extend: {},
