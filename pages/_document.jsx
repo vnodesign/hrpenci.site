940745af01fc60bacd186b35baf16e8648df7f28
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-03-13 22:21:08 +0700

    Update package

---
+++ pages/_document.jsx
@@ -0,0 +1,43 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import * as fs from 'fs'
+import * as path from 'path'
+
+class InlineStylesHead extends Head {
+  getCssLinks(files) {
+    return files.sharedFiles
+      .filter((file) => /\.css$/.test(file))
+      .filter((file) => fs.existsSync(path.join(process.cwd(), '.next', file)))
+      .map((file) => (
+        <style
+          key={file}
+          nonce={this.props.nonce}
+          data-href={`${this.context.assetPrefix}/_next/${file}`}
+          dangerouslySetInnerHTML={{
+            __html: fs.readFileSync(
+              path.join(process.cwd(), '.next', file),
+              'utf-8'
+            ),
+          }}
+        />
+      ))
+  }
+}
+
+export default class Document extends NextDocument {
+  static async getInitialProps(ctx) {
+    const initialProps = await NextDocument.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="vi" prefix="og: https://ogp.me/ns#">
+        <InlineStylesHead />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
