22df34b9d4d404089d75ddc3909b394aea79609a
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-03-22 09:52:28 +0700

    Update DocsSwitcher.tsx

--- components/DocsSwitcher.tsx
+++ components/DocsSwitcher.tsx
@@ -2,65 +2,65 @@ import cn from 'clsx'
 import { useRouter } from 'next/router'
 import Link from './Link'
 
-type DocSite = 'blog' | 'docs'
+export type DocSite = 'blog' | 'docs';
 
-export function useDocSite(): DocSite | undefined {
+export function useDocsSite(): DocSite | undefined {
   const { pathname } = useRouter()
-  const site: DocSite | undefined = pathname.startsWith('/docs')
-    ? 'docs'
-    : pathname.startsWith('/blog')
-    ? 'blog'
-    : undefined
-  return site
-}
 
-type DocsSiteSwitcherLinkProps = {
-  href: string
-  text: string
-  isActive?: boolean
-}
-
-function DocsSiteSwitcherLink({ href, text, isActive }: DocsSiteSwitcherLinkProps) {
-  const classes =
-    'py-1 transition-colors duration-300 inline-block w-[50px] cursor-pointer hover:text-black dark:hover:text-white'
-
-  const conditionalClasses = {
-    'text-black dark:text-white': isActive,
+  if (pathname.startsWith('/docs')) {
+    return 'docs'
   }
 
-  return (
-    <Link href={href} className={cn(classes, conditionalClasses)} title={text}>
-      {text}
-    </Link>
-  )
+  if (pathname.startsWith('/blog')) {
+    return 'blog'
+  }
+
+  return undefined;
 }
 
-function DocsSwitcher() {
-  const site = useDocSite()
+function DocsSiteSwitcherLink({ href, text, isActive }) {
+  const classes =
+    'py-1 transition-colors duration-300 inline-block w-[50px] cursor-pointer hover:text-black dark:hover:text-white';
+
+  const conditionalClasses = {
+    'text-black dark:text-white': !!isActive,
+  };
 
   return (
-    <div className="relative flex items-center justify-between p-2 text-xl group">
+    <Link href={href} className={cn(classes, conditionalClasses)}>
+      {text}
+    </Link>
+  );
+}
+
+function DocsSiteSwitcher() {
+  const site = useDocsSite()
+
+  return (
+    <div className='relative flex items-center justify-between p-2 text-xl group'>
       <span
         className={cn(
           'flex h-[34px] w-[100px] flex-shrink-0 items-center rounded-[8px] border border-[#dedfde] dark:border-[#333333] p-1 duration-300 ease-in-out',
           'after:h-[24px] after:w-[44px] after:rounded-md dark:after:bg-[#333333] after:shadow-sm after:duration-300 after:border dark:after:border-[#333333] after:border-[#666666]/100 after:bg-gradient-to-b after:from-[#3286F1] after:to-[#C33AC3] after:opacity-20 dark:after:opacity-100 dark:after:bg-none',
-          { 'indeterminate:after:hidden': !site, 'after:translate-x-[46px]': site === 'blog' }
+          'indeterminate:after:hidden',
+          {
+            'after:hidden': !site,
+            'after:translate-x-[46px]': site === 'pack',
+          }
         )}
       />
 
-      {site && (
-        <span
-          className={cn(
-            'z-50 absolute p-1 text-sm flex justify-between text-center w-[100px] text-[#666666] dark:text-[#888888]',
-            { 'hover:text-black dark:hover:text-white': site }
-          )}
-        >
-          <DocsSiteSwitcherLink href="/docs" text="Docs" isActive={site === 'docs'} />
-          <DocsSiteSwitcherLink href="/blog" text="Blog" isActive={site === 'blog'} />
-        </span>
-      )}
+      <span
+        className={cn(
+          'z-50 absolute p-1 text-sm flex justify-between text-center w-[100px] text-[#666666] dark:text-[#888888]',
+          { 'hover:text-black dark:hover:text-white': site }
+        )}
+      >
+        <DocsSiteSwitcherLink href='/docs' text='Docs' isActive={site === 'docs'} />
+        <DocsSiteSwitcherLink href='/blog' text='Blog' isActive={site === 'blog'} />
+      </span>
     </div>
-  )
+  );
 }
 
-export default DocsSwitcher
+export default DocsSiteSwitcher
