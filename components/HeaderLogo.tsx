a0de8c2e53784e743c945f0d0f9ca172bef16269
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-03-22 09:34:15 +0700

    Update HeaderLogo.tsx

--- components/HeaderLogo.tsx
+++ components/HeaderLogo.tsx
@@ -5,8 +5,8 @@ import styles from './header-logo.module.scss'
 function HeaderLogo() {
   return (
     <>
-      <Link href="/" title="HR Documentation" className="font-semibold">
-        HR Documentation
+      <Link href="/" title="HR Document" className="font-semibold">
+        HR Document
       </Link>
       <div className={styles.siteSwitcher}>
         <DocsSwitcher />
