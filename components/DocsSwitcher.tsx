35d3cb1b57ba60c8511e3398682244d8f0b6fc17
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-03-22 09:40:17 +0700

    Update DocsSwitcher.tsx

--- components/DocsSwitcher.tsx
+++ components/DocsSwitcher.tsx
@@ -43,7 +43,7 @@ function DocsSwitcher() {
       <span
         className={cn(
           'flex h-[34px] w-[100px] flex-shrink-0 items-center rounded-[8px] border border-[#dedfde] dark:border-[#333333] p-1 duration-300 ease-in-out',
-          'after:h-[24px] after:w-[44px] after:rounded-full dark:after:bg-[#333333] after:shadow-sm after:duration-300 after:border dark:after:border-[#333333] after:border-[#666666]/100 after:bg-gradient-to-b after:from-[#3286F1] after:to-[#C33AC3] after:opacity-20 dark:after:opacity-100 dark:after:bg-none',
+          'after:h-[24px] after:w-[44px] after:rounded-md dark:after:bg-[#333333] after:shadow-sm after:duration-300 after:border dark:after:border-[#333333] after:border-[#666666]/100 after:bg-gradient-to-b after:from-[#3286F1] after:to-[#C33AC3] after:opacity-20 dark:after:opacity-100 dark:after:bg-none',
           { 'indeterminate:after:hidden': !site, 'after:translate-x-[46px]': site === 'blog' }
         )}
       />
@@ -51,8 +51,8 @@ function DocsSwitcher() {
       {site && (
         <span
           className={cn(
-            'z-50 absolute p-1 text-sm flex justify-between text-center w-[100px] text-slate-700 dark:text-slate-300',
-            { 'text-slate-900 dark:text-white': site }
+            'z-50 absolute p-1 text-sm flex justify-between text-center w-[100px] text-[#666666] dark:text-[#888888]',
+            { 'hover:text-black dark:hover:text-white': site }
           )}
         >
           <DocsSiteSwitcherLink href="/docs" text="Docs" isActive={site === 'docs'} />
