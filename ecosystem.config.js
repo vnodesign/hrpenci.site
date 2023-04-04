cbf6624bd958691c46fd3637733d41041a57df72
Tuan Duc Design <69758022+tuanducdesign@users.noreply.github.com>
2023-04-05 02:00:06 +0700

    Update deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,27 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'HR',
-      append_env_to_name: true,
-      script: './node_modules/next/dist/bin/next start',
-      max_memory_restart: '2G',
+      script: 'yarn',
+      args: 'start',
       env: {
         NODE_ENV: 'production',
       },
     },
   ],
-
-  deploy: {
-    staging: {
-      user: 'root',
-      key: '~/.ssh/tuanducdesign',
-      host: '14.225.204.116',
-      ref: 'origin/production',
-      repo: 'git@github.com:tuanducdesign/hr.penci.me.git',
-      path: '/www/wwwroot/hr.penci.me',
-      'post-deploy': 'sh pm2-deploy.sh',
-      env: {
-        NODE_ENV: 'production',
-      },
-    },
-  },
 }
