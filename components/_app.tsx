f780affe6877f4f0ae689ec3fe42e401b36360fc
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-04-08 02:52:39 +0700

    Update _app.tsx

--- components/_app.tsx
+++ components/_app.tsx
@@ -5,13 +5,13 @@ import CopyLink from './CopyLink'
 import { TailwindIndicator } from './tailwind-indicator'
 
 export function reportWebVitals(metric: NextWebVitalsMetric) {
-  if (metric.label === 'web-vital') {
-    window.gtag('event', name, {
+  if (typeof window !== 'undefined' && metric.label === 'web-vital') {
+    window.gtag('event', metric.name, {
       event_category: 'web-vital',
       value: Math.round(metric.name === 'CLS' ? metric.value * 1000 : metric.value), // values must be integers
       event_label: metric.id, // id unique to current page load
       non_interaction: true, // avoids affecting bounce rate.
-    })  
+    })
   }
 }
 
