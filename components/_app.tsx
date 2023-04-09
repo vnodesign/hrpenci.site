2ec0626aa44f658c42a44ad596c5efb117f14246
Tuan Duc Design <69758022+tuanducdesign@users.noreply.github.com>
2023-04-10 01:46:43 +0700

    Update any pages

--- components/_app.tsx
+++ components/_app.tsx
@@ -1,10 +1,9 @@
-import type { AppProps, NextWebVitalsMetric } from 'next/app'
 import { SSRProvider } from '@react-aria/ssr'
 import { Scripts } from './Scripts'
 import CopyLink from './CopyLink'
 import { TailwindIndicator } from './tailwind-indicator'
 
-export default function Nextra({ Component, pageProps }: AppProps) {
+export default function Nextra({ Component, pageProps }) {
   return (
     <>
       <SSRProvider>
