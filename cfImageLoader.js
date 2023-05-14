9aaf6c68ba4ddabe03a3fe84e3410bb10eb5af40
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-05-14 21:49:14 +0700

    Update cfImageLoader.js

--- cfImageLoader.js
+++ cfImageLoader.js
@@ -1,4 +1,5 @@
-export default function cfImageLoader({ src, width, quality }) {
-    const params = [`width=${width}`, `quality=${quality || 75}`, 'format=auto']
-    return `https://hrpenci.site/cdn-cgi/image/${params.join(',')}/${src}`
+// Docs: https://developers.cloudflare.com/images/url-format
+export default function cloudflareLoader({ src, width, quality }) {
+  const params = [`width=${width}`, `quality=${quality || 75}`, 'format=auto'];
+  return `https://hrpenci.site/cdn-cgi/image/${params.join(',')}/${src}`;
 }
