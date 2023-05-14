241626d311541c33ca0a2cd4f8b3dc3ec5cbd323
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-05-15 02:02:31 +0700

    Update imagekitLoader.js

--- imagekitLoader.js
+++ imagekitLoader.js
@@ -1,9 +1,6 @@
 export default function imageKitLoader({ src, width, quality }) {
   if(src[0] === '/') src = src.slice(1);
-  const params = [`w-${width}`];
-  if (quality) {
-    params.push(`q-${quality}`);
-  }
+  const params = [`w-${width}`, `q-${quality || 75}`];
   const paramsString = params.join(',');
   var urlEndpoint = 'https://ik.imagekit.io/vnodesign/hrpenci';
   if(urlEndpoint[urlEndpoint.length-1] === '/') urlEndpoint = urlEndpoint.substring(0, urlEndpoint.length - 1);
