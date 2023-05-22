140ced13c1bdba2934a8ce300bebe15f4ff10bab
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-05-23 00:37:08 +0700

    Update UMScript.tsx

--- components/Scripts/UMScript.tsx
+++ components/Scripts/UMScript.tsx
@@ -6,8 +6,6 @@ export function UMScript() {
       <Script
         src="https://analytics.umami.is/script.js"
         data-website-id="b391f518-ab8a-47bc-b914-d00bb7f23386"
-        async
-        defer
       />
     </>
   )
