83d747eca08fd6e2036e03978a433fc0eee9772f
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-05-23 00:56:50 +0700

    Update index.tsx

--- components/Scripts/index.tsx
+++ components/Scripts/index.tsx
@@ -1,17 +1,11 @@
 import { GAScript } from './GAScript'
-import { UMScript } from './UMScript'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 export function Scripts() {
   return (
     <>
-      {isProduction && (
-        <>
-          <GAScript />
-          <UMScript />
-        </>
-      )}
+      {isProduction && <GAScript />}
     </>
   )
 }
