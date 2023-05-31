8c6eb02542b041394693d0747b7d66473490b708
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-05-31 23:55:54 +0700

    Update next-sitemap.config.js
    Block cdn-cgi from CloudFlare

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,15 @@
 module.exports = {
   siteUrl: 'https://hrpenci.site',
   generateRobotsTxt: true,
+  exclude: ['/cdn-cgi'],
+  generateIndexSitemap: true,
+  sitemapSize: 500,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
 }
