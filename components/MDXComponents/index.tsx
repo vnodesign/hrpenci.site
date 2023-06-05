f969de14b5a5f94747be6ef643190e001d1e8200
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-06-05 20:53:08 +0700

    Update index.tsx

--- components/MDXComponents/index.tsx
+++ components/MDXComponents/index.tsx
@@ -1,35 +1,10 @@
-import cn from 'clsx'
 import { Callout } from '../Callout'
-import Link from '../Link'
 import { Tab, Tabs } from '../Tabs'
 import { Steps } from '../Steps'
-import { Code } from '../Code'
-import { Pre } from '../Pre'
 
 export const MDXComponents = {
   Callout: (props) => <Callout {...props} />,
-  a: (props) => (
-    <Link
-      {...props}
-      className="font-semibold !no-underline !border-b-[1px] !border-b-primary-600 hover:!border-b-2 !rounded-none"
-    />
-  ),
   Tab: (props) => <Tab {...props} />,
   Tabs: (props) => <Tabs {...props} />,
   Steps: (props) => <Steps {...props} />,
-  pre: Pre,
-  code: Code,
-  blockquote: (props) => (
-    <blockquote
-      className={cn(
-        'sm:my-[1.3333333333em] my-[1.6em] lg:my-[1.6666666667em] xl:my-[1.6em] 2xl:my-[1.7777777778em] border-gray-300 italic text-gray-700 dark:border-gray-700 dark:text-gray-400',
-        'first:my-0 border-l-[0.25em] sm:pl-[1.1111111111em] pl-[1em] lg:pl-[1em] xl:pl-[1.0666666667em] 2xl:pl-[1.1111111111em] font-medium'
-      )}
-      {...props}
-    />
-  ),
-  p: (props) => <p {...props} className="leading-7 [&:not(:first-child)]:mt-6" />,
-  ul: (props) => <ul className="my-6 ml-6 list-disc" {...props} />,
-  ol: (props) => <ol className="my-6 ml-6 list-decimal" {...props} />,
-  li: (props) => <li className="mt-2" {...props} />,
 }
