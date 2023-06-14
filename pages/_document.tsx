6dd714315bac849ffdbc73ee01ef912a52879d52
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-06-14 08:50:53 +0700

    Add Meticulous AI

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,6 +37,14 @@ export default class MyDocument extends Document {
             }`
             }}
           />
+          {(process.env.NODE_ENV === 'development' ||
+            process.env.VERCEL_ENV === 'preview') && (
+            // eslint-disable-next-line @next/next/no-sync-scripts
+            <script
+              data-project-id="nkC8HtJbDunyxQdEBVKtqw74mbCwoD6cVCftDVmM"
+              src="https://snippet.meticulous.ai/v1/stagingMeticulousSnippet.js"
+            />
+          )}
         </Head>
         <body>
           <Main />
