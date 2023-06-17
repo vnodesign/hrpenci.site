a403173a983a5d9661e6481f49ca0bd3532b0712
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-06-17 10:05:00 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,8 +10,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  workboxPluginMode: 'InjectManifest'
+  disable: process.env.NODE_ENV === 'development'
 })
 
 const nextConfig = withNextra(
