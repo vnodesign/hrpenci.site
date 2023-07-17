5778dd902521e2098f0773250992ca65931d1e84
Tuan Duc Tran <nguoisoidangyeu@outlook.com.vn>
2023-07-18 01:08:03 +0700

    Update _app.tsx

--- components/_app.tsx
+++ components/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from 'next/app'
+import type { ReactNode } from 'react'
 import { Scripts } from './Scripts'
 
-export default function Nextra({ Component, pageProps }: AppProps) {
+type NextraAppProps = AppProps & {
+  Component: AppProps['Component'] & {
+    getLayout: (page: ReactNode) => ReactNode
+  }
+}
+
+export default function Nextra({ Component, pageProps }: NextraAppProps) {
   return (
     <>
       <Component {...pageProps} />
