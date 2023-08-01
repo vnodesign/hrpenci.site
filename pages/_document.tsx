be253df71f0bf171b11277790ce2c362dcbf42f7
Tuan Duc Tran <nguoisoidangyeu@outlook.com.vn>
2023-08-02 01:04:08 +0700

    Remove getInitialProps from _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,14 @@
 import { siteConfig } from '@data/siteConfig'
-import Document, { Head, Html, Main, NextScript } from 'next/document'
-import type { DocumentContext, DocumentInitialProps } from 'next/document'
-
-export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
-  render() {
-    return (
-      <Html lang={siteConfig.language} prefix="og: https://ogp.me/ns#">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+import { Html, Head, Main, NextScript } from 'next/document'
+ 
+export default function Document() {
+  return (
+    <Html lang={siteConfig.language} prefix="og: https://ogp.me/ns#">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
