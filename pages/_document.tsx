62954da1039280907510dc10853b9f68d4aee029
Tuan Duc Tran <nguoisoidangyeu@outlook.com.vn>
2023-08-13 00:20:02 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,43 @@
 import { siteConfig } from '@data/siteConfig'
-import Document, { Head, Html, Main, NextScript } from 'next/document'
-import type { DocumentContext, DocumentInitialProps } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
+import fs from 'fs'
+import path from 'path'
 
-export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+type DocumentFiles = {
+  sharedFiles: readonly string[]
+}
 
-  render() {
-    return (
-      <Html lang={siteConfig.language} prefix="og: https://ogp.me/ns#">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
+class InlineStylesHead extends Head {
+  getCssLinks(files: DocumentFiles) {
+    return files.sharedFiles
+      .filter((file) => /\.css$/.test(file))
+      .filter((file) =>
+        fs.existsSync(path.join(process.cwd(), '.next', file))
+      )
+      .map((file) => (
+        <style
+          key={file}
+          nonce={this.props.nonce}
+          data-href={`${this.context.assetPrefix}/_next/${file}`}
+          dangerouslySetInnerHTML={{
+            __html: fs.readFileSync(
+              path.join(process.cwd(), '.next', file),
+              'utf-8'
+            ),
+          }}
+        />
+      ));
   }
+}
+
+export default function Document() {
+  return (
+    <Html lang={siteConfig.language} prefix="og: https://ogp.me/ns#">
+      <InlineStylesHead />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
