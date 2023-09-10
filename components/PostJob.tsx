692edb229c9c4968accc968b06139b9872a82f72
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-09-10 11:58:25 +0700

    Add file preview

--- components/PostJob.tsx
+++ components/PostJob.tsx
@@ -5,7 +5,7 @@ export default function PostJob() {
   return (
     <Link
       href="/dang-tin-tuyen-dung"
-      className="inline-flex items-center transition-colors bg-vnodesign-100 text-vnodesign-800 dark:bg-vnodesign-400/10 dark:text-vnodesign-600 contrast-more:border contrast-more:border-vnodesign-500 contrast-more:dark:border-vnodesign-500 font-semibold rounded-lg text-sm px-3 py-2 text-center"
+      className="inline-flex items-center text-gray-900 bg-gray-200 dark:bg-vnodesign-100/10 dark:text-gray-50 hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-vnodesign-100/5 dark:hover:text-gray-50 font-semibold rounded-lg text-sm px-3 py-2 text-center"
       title="Đăng tin tuyển dụng"
       onClick={() =>
         gtagTrack(
--- components/_app.tsx
+++ components/_app.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/no-unknown-property */
 import type { AppProps } from 'next/app'
 import { Fira_Code, Inter } from 'next/font/google'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 import { Scripts } from './Scripts'
 
 const inter = Inter({
@@ -14,6 +16,10 @@ const firaCode = Fira_Code({
   subsets: ['latin'],
   variable: '--font-fira-code'
 })
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
