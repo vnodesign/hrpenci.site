9ae916dc131a42bda32bcff9a288e9da05abede9
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-09-11 02:04:49 +0700

    Optimize UX/UI

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,11 @@
-import ProgressBar from '@badrap/bar-of-progress'
 import { siteConfig } from '@data/siteConfig'
 import { Head, Html, Main, NextScript } from 'next/document'
-import Router from 'next/router'
-
-const progress = new ProgressBar({
-  size: 2,
-  color: '#38bdf8',
-  className: 'bar-of-progress',
-  delay: 100
-})
-
-// this fixes safari jumping to the bottom of the page
-// when closing the search modal using the `esc` key
-if (typeof window !== 'undefined') {
-  progress.start()
-  progress.finish()
-}
-
-Router.events.on('routeChangeStart', () => progress.start())
-Router.events.on('routeChangeComplete', () => progress.finish())
-Router.events.on('routeChangeError', () => progress.finish())
 
 export default function Document() {
   return (
     <Html lang={siteConfig.language} prefix="og: https://ogp.me/ns#">
       <Head />
-      <body className="antialiased">
+      <body>
         <Main />
         <NextScript />
       </body>
