b6401abb5c7209249dcc5ea40cb3615f02f90fa8
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-10-10 15:37:10 +0700

    Upgrade packages

--- components/_app.tsx
+++ components/_app.tsx
@@ -3,15 +3,18 @@ import { BrandJsonLd, LogoJsonLd, SocialProfileJsonLd } from 'next-seo'
 import type { AppProps } from 'next/app'
 import Router from 'next/router'
 import NProgress from 'nprogress'
+import { useEffect } from 'react'
 import { Scripts } from './Scripts'
 
-NProgress.configure({ showSpinner: false })
-
-Router.events.on('routeChangeStart', () => NProgress.start())
-Router.events.on('routeChangeComplete', () => NProgress.done())
-Router.events.on('routeChangeError', () => NProgress.done())
-
 export default function App({ Component, pageProps }: AppProps) {
+  NProgress.configure({ showSpinner: false })
+
+  useEffect(() => {
+    Router.events.on('routeChangeStart', () => NProgress.start())
+    Router.events.on('routeChangeComplete', () => NProgress.done())
+    Router.events.on('routeChangeError', () => NProgress.done())
+  })
+
   return (
     <>
       <SocialProfileJsonLd
