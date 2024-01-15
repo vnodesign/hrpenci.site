04dd85ac5b3a077c683ce085928c3b4571766011
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-01-15 21:51:16 +0700

    Update sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/nextjs'
+import { ProfilingIntegration } from '@sentry/profiling-node'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
@@ -6,7 +7,12 @@ Sentry.init({
   dist: '1',
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
+  integrations: [
+    // Add profiling integration to list of integrations
+    new ProfilingIntegration()
+  ],
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false
